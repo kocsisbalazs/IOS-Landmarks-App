40bb971d87c04680498a3914b65e5f164bbc1f2f
Balazs Kocsis <balazskocsis@gmail.com>
2023-11-09 18:24:41 +0100

    asd

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -11,20 +11,39 @@ import SwiftUI
 // var Body represent a property
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
+        VStack {
+            MapView()
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rocks")
+                    .font(.title2)
+                Text("Turtle Rocks in the national park stand as ancient sentinels, their weathered formations resembling the rugged shells of colossal turtles.")
             }
+            .padding()
+            
+            Spacer()
         }
-        .padding()
     }
 }
+// It's all about chaining the modifiers :)
 
 #Preview {
     ContentView()
